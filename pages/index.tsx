09eec7ba0a2efa32843ef51be06aa2ab811dad00
andriiokhrimenko.github.io <myindexukrnet@gmail.com>
2023-05-14 17:37:24 +0300

    Finishing post component at home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Header from '@components/Header'
 import Head from 'next/head'
-import { sanityClient } from '../sanity'
+import { sanityClient, urlFor } from '../sanity'
 import { Post } from './../typings';
 import Layout from './_app'
 import Link from 'next/link';
@@ -12,8 +12,8 @@ interface Props{
 
 
 export default function Home({ posts }: Props) {
-  console.log('render home')
-  console.log(JSON.stringify(posts))
+  // console.log('render home')
+  // console.log(JSON.stringify(posts))
 
   return (
     <div className="bg-white text-black max-w-7xl mx-auto">
@@ -36,16 +36,29 @@ export default function Home({ posts }: Props) {
           src="https://cdn.iconscout.com/icon/free/png-256/free-medium-47-433328.png" alt="" />
       </div>
 
-      <div>
-
+      <div className=
+        'grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-6'>
         {posts?.map((post) => (
           <Link key={post._id} href={`/post/${post.slug.current}`}>
-            <div>
-              <h1>I am post</h1>
+            <div className='group cursor-pointer border rounded-lg overflow-hidden'>
+              {post.mainImage && (
+                <img
+                  className='h-60 w-full object-cover group-hover:scale-105 
+                  transition-transform duration-200 ease-in-out'
+                  src={urlFor(post.mainImage).url()} alt="" />
+              )}
+              <div className='flex justify-between p-5 bg-white'>
+                <div>
+                  <p className='text-lg font-bold'>{post.title}</p>
+                  <p className='text-xs'>{post.description} by {post.author?.name}</p>
+                </div>
+                <img
+                  className='h-12 w-12 rounded-full'
+                  src={urlFor(post.author.image).url()!} alt="" />
+              </div>
             </div>
           </Link>
         ))}
-
       </div>
       
     </div>
@@ -56,24 +69,21 @@ Home.Layout=Layout
 
 
 export const getServerSideProps = async () => {
-  console.log('render get ssr')
+  // console.log('render get ssr')
   
    const query = 
   `*[_type == "post"]{
     _id,
     title,
-    author =>{
-      name,
-      image
-    },
     description,
     mainImage,
     slug,
+    author-> {name,image}
   }`;
 
   const posts = await sanityClient.fetch(query)
 
-  console.log(posts)
+  // console.log(posts[0].author.name)
 
   return {
     props: { posts }
